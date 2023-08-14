9b9613c11a763bbc30e50335bb14914e122f5d11
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2023-08-13 19:09:55 -0500

    Update api

--- app.js
+++ app.js
@@ -1,122 +1,25 @@
-const express = require('express');
-const crypto = require('node:crypto');
-const cors = require('cors');
-const movies = require('./movies.json');
-const { validateMovie, validatePartialMovie } = require('./schemas/movies');
+import express, { json } from 'express' // require -> commonJS
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
+// EN EL FUTURO: el import del json será así:
+// import movies from './movies.json' with { type: 'json' }
 
+// como leer un json en ESModules
+// import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
 
-const app = express();
-app.use(express.json());//Por defecto le dice que si a todo (*)
-app.use(cors({
-    origin: (origin, callback) => {
-        const ACCEPTED_ORIGINS = [
-            'http://localhost:8080',
-            'http://localhost:1234/',
-            'https://movies.com' // Produccion o desarrollo
-        ]
-        if (ACCEPTED_ORIGINS.includes(origin)) {
-            callback(null, true);
-        }
-        if(!origin) {
-            callback(null, true);
-        }
-        return callback(new Error('Not allowed by CORS'));
-    }
-}));
-app.disable('x-powered-by'); // Deshabilitar la cabecera X-Powered-By: Express
+// como leer un json en ESModules recomandado por ahora
 
-// Metodos normales: GET/HEAD/POST
-// Metodos complejos: PUT/PATCH/DELETE
+const app = express()
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by')
 
-// CORS PRE-Flight
-// OPTIONS
+app.use('/movies', moviesRouter)
 
-
-const ACCEPTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://localhost:1234/',
-    'https://movies.com' // Produccion o desarrollo
-];
-
-app.get('/movies', (req, res) => {
-    const { genre } = req.query;
-    if (genre) {
-        const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()));
-        return res.json(filteredMovies);
-    }
-    res.json(movies);
-});
-
-app.get('/movies/:id', (req, res) => {
-    const { id } = req.params;
-    const movie = movies.find(movie => movie.id === id);
-    if (movie) return res.json(movie);
-    res.status(404).json({
-        message: 'Movie not found'
-    });
-});
-
-app.post('/movies', (req, res) => {
-    const result = validateMovie(req.body);
-
-    if (result.error) {
-        return res.status(400).json({ error: JSON.parse(result.error.message) });
-    }
-
-    const newMovie = {
-        id: crypto.randomUUID(), // uuid v4
-        ...result.data
-    };
-
-    movies.push(newMovie);
-
-    res.status(201).json(newMovie); // 201 CREATED
-});
-
-app.patch('/movies/:id', (req, res) => {
-    const result = validatePartialMovie(req.body);
-
-    if (!result.success) {
-        return res.status(400).json({ error: JSON.parse(result.error.message) });
-    }
-
-    const { id } = req.params;
-    const movieIndex = movies.findIndex(movie => movie.id === id);
-
-    if (movieIndex === -1) {
-        return res.status(404).json({
-            message: 'Movie not found'
-        });
-    }
-
-    const updateMovie = {
-        ...movies[movieIndex],
-        ...result.data
-    };
-
-    movies[movieIndex] = updateMovie;
-    res.json(updateMovie);
-});
-
-app.delete('/movies/:id', (req, res) => {
-    const { id } = req.params;
-    const movieIndex = movies.findIndex(movie => movie.id === id);
-
-    if (movieIndex === -1) {
-        return res.status(404).json({
-            message: 'Movie not found'
-        });
-    }
-
-    movies.splice(movieIndex, 1);
-
-    return res.json({ message: 'Movie deleted' });
-});
-
-
-const PORT = process.env.PORT || 1234;
+const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, () => {
-    console.log(`Listening on port http://localhost:${PORT}`);
-});
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
--- schemas/movies.js
+++ schemas/movies.js
@@ -1,33 +1,30 @@
-const z = require('zod')
+import z from 'zod'
 
 const movieSchema = z.object({
-    title: z.string({
-        required_error: 'Title is required',
-        required_error: 'Title is required'
-    }),
-    year: z.number().int().min(1900).max(2024),
-    director: z.string(),
-    duration: z.number().int().positive(),
-    rate: z.number().int().min(0).max(10).default(5),
-    poster: z.string().url({
-        message: 'Poster must be a valid URL'
-    }),
-    genre: z.array(z.enum(['Action', 'Adventure', 'Drama', 'Horror', 'Sci-Fi', 'Thriller', 'Comedy', 'Romance', 'Crime', 'Animation']), {
-        required_error: 'Genre is required',
-        invalid_type_error: 'Genre must be an array of strings'
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required.'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
     }
-    )
+  )
 })
 
-function validateMovie (input) {
-    return movieSchema.safeParse(input)//devuelve un objeto result si hay error o hay datos 
+export function validateMovie (input) {
+  return movieSchema.safeParse(input)
 }
 
-function validatePartialMovie (input) {
-    return movieSchema.partial().safeParse(input)
-}
-
-module.exports = {
-    validateMovie,
-    validatePartialMovie
+export function validatePartialMovie (input) {
+  return movieSchema.partial().safeParse(input)
 }
